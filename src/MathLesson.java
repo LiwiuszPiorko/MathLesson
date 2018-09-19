9bdde846c5ca6d91681451d62cd918041731423b
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-19 13:04:47 +0200

    Pierwsza wersja

---
+++ src/MathLesson.java
@@ -0,0 +1,10 @@
+public class MathLesson {
+
+    public static void main(String[]args){
+
+    Triangle triangle1= new Triangle(5.5,6.6, 7.7);
+    Method check= new Method();
+    boolean isRightTriangle = check.isRightTriangle(triangle1);
+
+    }
+}
---
+++ src/Method.java
@@ -0,0 +1,29 @@
+public class Method {
+
+    public boolean isRightTriangle(Triangle triangle1) {
+
+        boolean eqtriangle = triangle1.a * triangle1.a + triangle1.b * triangle1.b == triangle1.c * triangle1.c;
+
+        if (eqtriangle) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
